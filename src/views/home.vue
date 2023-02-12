c2cc5473235c4dcc559a89523e1611dbf66ef177
nan[0] <56279981+nonobstant@users.noreply.github.com>
2023-02-12 22:31:47 +0100

    init

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="m-6" v-for="(link, index) in links" :key="index">
-      <a class=" px-2 py-1  text-black font-bold text-2xl cursor-crosshair hover:text-white hover:bg-black dark:text-white dark:hover:text-black dark:hover:bg-white" :href="link.url">{{ link.text }} </a>
+    <div class="grid grid-cols-7 gap-1 w-80 mx-auto mt-6">
+      <div v-for='(_case, index) in board' :key="index" class="w-10 h-10">
+        <div v-if="isPair(index)" class="bg-slate-800 dark:bg-purple-300 w-full h-full"></div>
+        <div v-else class="bg-slate-200 dark:bg-purple-700 w-full h-full"></div>
+      </div>
     </div>
 </template>
 
@@ -8,38 +11,25 @@
 export default {
   data() {
     return {
-      links : [
-        {
-          text : "LUX MINIMA",
-          url : "https://luxminima.xyz",
-          img : "./assets/"
-        },
-        {
-          text : "PRISM FLOWER",
-          url : "https://prismflower.xyz",
-          img : "./assets/"
-        },
-        {
-          text : "ATELIER404",
-          url : "https://atelier404.xyz",
-          img : "./assets/"
-        },
-        {
-          text : "TWITTER",
-          url : "#",
-          img : "./assets/"
-        },
-        {
-          text : "INSTAGRAM",
-          url : "#",
-          img : "./assets/"
-        },
-      ]
+      board : []
     }
   },
   methods : {
+    isPair(num) {
+      if (num%2 == 0) {
+        return true
+      } else {
+        return false
+      }
+    }
   },
   mounted() {
+    for (let index = 0; index < 49; index++) {
+      var _case = {
+        num : index
+      }
+      this.board.push(_case)
+    }
   }
 }
 </script>
